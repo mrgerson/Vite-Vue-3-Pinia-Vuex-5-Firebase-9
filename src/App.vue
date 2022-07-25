3e94fc2d35a6326b70f128185bf9d2a417ecff1c
Gerson <gedacaba@gmail.com>
2022-07-25 13:34:17 -0500

    configuración inicial

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>App1</h1>
+    <nav>
+      <router-link to="/" >Home</router-link> |
+      <router-link to="/login" >Login</router-link> |
+      <router-link to="/register" >Register</router-link> |
+      <button>Logout</button>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
